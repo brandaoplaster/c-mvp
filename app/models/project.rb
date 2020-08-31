98aeaa6ecf6285887204aaebf7a46b28ced6d649
brandaoplaster <brandaoplaster@gmail.com>
2020-08-30 22:24:50 -0300

    Instalado active storage e criado migrate

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,5 @@ class Project < ApplicationRecord
   validates :title, :description, :target_value, :end_date, presence: true
   validates :description, length: { maximum: 512 }
   validates :title, length: { maximum: 64 }
+  has_one_attached :image_project
 end
