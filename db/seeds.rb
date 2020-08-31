8ed179a3d544c6976d4f6ad5af86fb94a8e272fb
brandaoplaster <brandaoplaster@gmail.com>
2020-08-31 00:19:08 -0300

    Criando seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+images_dir = Rails.root.join("db/seed_images")
+
+user1 = User.create(name: 'Pedro', email: 'pedro@test.com', password: 123456)
+user2 = User.create(name: 'Ana', email: 'ana@test.com', password: 123456)
+user3 = User.create(name: 'Karla', email: 'karla@test.com', password: 123456)
+user4 = User.create(name: 'Breno', email: 'breno@test.com', password: 123456)
+user5 = User.create(name: 'Jessica', email: 'jessica@test.com', password: 123456)
+
+project = Project.create(title: 'Revista em quadrinho', description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s', target_value: 300.00, end_date: DateTime.new(2020,8,30), user: user1)
+
+project.image_project.attach(io: File.open("#{images_dir}/project_1.jpg"), filename: 'project_1.jpg')
+
+Supporter.create(user: user2, project: project, value: 100.00 )
+Supporter.create(user: user3, project: project, value: 50.00 )
