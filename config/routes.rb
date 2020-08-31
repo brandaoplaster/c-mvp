54a8543e2ee4e52e48498a4199cfd54bbffabb60
brandaoplaster <brandaoplaster@gmail.com>
2020-08-31 11:17:34 -0300

    Criando controllers dashboard e pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root to: "pages#index"
+  end
 end
