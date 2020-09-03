f72b092b9ad5ff31f08033b72bb99d7fcc273515
brandaoplaster <brandaoplaster@gmail.com>
2020-09-02 21:40:40 -0300

    Criando um novo projeto

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,7 +9,19 @@ class ProjectsController < ApplicationController
 
   end
 
+  def new
+    @project = Project.new
+  end
+
   def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Contact succesfully created"
+      redirect_to projects_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -19,6 +31,10 @@ class ProjectsController < ApplicationController
   end
 
   private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :target_value, :end_date, :image_project).merge(user: current_user)
+  end
 
   def set_projetc
     @project = Project.find(params[:id])
