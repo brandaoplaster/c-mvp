f3e09cd6a54b6d1b32a83aa127fa55408aad7cb7
brandaoplaster <brandaoplaster@gmail.com>
2020-09-05 14:49:44 -0300

    Criado helper para formata data e valor monetario

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -6,6 +6,14 @@ module PagesHelper
         total += supporter.donated_amount
       end
     end
-    total
+    number_to_currency(total, unit: "R$", separator: ",", delimiter: "")
+  end
+
+  def format_monetary_value(value)
+    number_to_currency(value, unit: "R$", separator: ",", delimiter: "")
+  end
+
+  def formart_date(date)
+    date.strftime("%d/%m/%Y")
   end
 end
