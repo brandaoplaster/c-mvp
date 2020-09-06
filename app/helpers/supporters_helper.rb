a77c846ed317cb03a40416405f489173510294ce
brandaoplaster <brandaoplaster@gmail.com>
2020-09-06 17:47:35 -0300

    Aprimorando a listagem de apoiadores

--- app/helpers/supporters_helper.rb
+++ app/helpers/supporters_helper.rb
@@ -1,2 +1,16 @@
 module SupportersHelper
+  def render_supporters(supporters)
+    list_supporters = supporters.uniq { |item| item[:user_id] }
+    count_supporters = list_supporters.count
+    list_supporters.each { |supporter| concat draw_list(supporter.user.name) }
+    count_supporters.times { draw_list("") }
+  end
+
+  private
+
+    def draw_list(name)
+      content_tag :div, class: "col s12 m12 blue-grey lighten-5" do
+        content_tag :span, name
+      end
+    end
 end
